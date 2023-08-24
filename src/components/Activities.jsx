05ae0f4ae694fe2b9904044c89c4d188f5f5b95f
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-24 11:00:15 +0300

    create the activity component  for resort

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Activities = () => {
   return (
-    <div className=" flex  justify-between mr-3">
-      <div className="w-screen rounded-lg shadow-md lg:max-w-sm">
-      <h1 className="mt-[-40px] text-white  p-[-24px]">Resorts</h1>
+    <div className="max-w-[1140px] m-auto w-full md:flex mt-[-75px]">
+      <div className="relative p-4">
+      <h1 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%] text-white text-2xl font-bold">Resorts</h1>
         <img
           className="w-full h-full object-cover relative border-4 border-white shadow-lg"
           src="https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
@@ -12,19 +12,11 @@ const Activities = () => {
         />
       </div>
 
-      <div className="w-screen rounded-lg shadow-md lg:max-w-sm">
-      <h1 className="mt-[-40px] text-white  p-[-24px]">Resorts</h1>
+      <div className="relative p-4">
+      <h1 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%] text-white text-2xl font-bold">Resorts</h1>
         <img
           className="w-full h-full object-cover relative border-4 border-white shadow-lg"
-          src="https://images.unsplash.com/photo-1599640842225-85d111c60e6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
-          alt="/"
-        />
-      </div>
-
-      <div className="w-screen rounded-lg shadow-md lg:max-w-sm">
-      <h1 className="mt-[-40px] text-white  p-[-24px]">Resorts</h1>
-        <img
-          className="w-full h-full object-cover relative border-4 border-white shadow-lg"
+          
           src="https://images.unsplash.com/photo-1599640842225-85d111c60e6b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
           alt="/"
         />
